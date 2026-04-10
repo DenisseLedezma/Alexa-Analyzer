a029cc82e7ba590eada94fecb788c3371726e5fb
DenisseLedezma <denisseledezma17@gmail.com>
2026-04-10 10:00:23 -0600

    Update IOTLambda.py

--- iotButton_Lambda/IOTLambda.py
+++ iotButton_Lambda/IOTLambda.py
@@ -17,7 +17,7 @@ def lambda_handler(event, context):
         deleteNews()
     
     return 'End of News Sentiment IOT function'
-
+// COMENTARIO
 
 def deleteNews():
     dynamodb = boto3.resource('dynamodb')
